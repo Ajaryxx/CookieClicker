0f093b8433cd0ad6f26b62eed85f23dc38e90534
Ajaryxx <impressum15@gmail.com>
2025-09-14 14:52:23 +0200

    added basic Project structure

---
+++ include/PCH.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iostream>
+#include <stdint.h>
+#include <vector>
+#include <memory>
+
+
+#include <SDL3/SDL.h>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
